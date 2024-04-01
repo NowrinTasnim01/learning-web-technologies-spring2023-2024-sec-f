6e76b5b4f13fe97ea7f213b1e8238d435a0481d7
Nowrin Tasnim <151002784+NowrinTasnim01@users.noreply.github.com>
2024-04-01 13:47:33 +0600

    Update contact.php

--- protfolio/contact.php
+++ protfolio/contact.php
@@ -8,8 +8,8 @@
 <body>
     <h2>Contact Information</h2>
 
-        <p>Name: Shakil Jamil Mrida</p>
-        <p>Email: mafialhasa@gmail.com</p>
+        <p>Name: Nowrin Tasnim</p>
+        <p>Email: nowrin2001@gmail.com</p>
         <p>Phone: +1234567890</p>
         <p>Address: Dhaka</p>
    
